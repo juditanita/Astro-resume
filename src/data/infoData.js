3d5f89c0c37427980416e2939616433683334a6b
juditanita <101408621+juditanita@users.noreply.github.com>
2024-05-09 01:10:04 +0200

    add education data

--- src/data/infoData.js
+++ src/data/infoData.js
@@ -1,4 +1,4 @@
-const infoData=[{
+export const infoData=[{
     id:"1",
 title : "Experience",
 name: ">2 years"
@@ -19,4 +19,29 @@ name: ">2 years"
             }
 ]
 
-export default infoData;
+export  const educationData=[
+    {
+    id:"1",
+    qualification:"Post Grad in Full Stack Developer", 
+    schoolProvider:"Caltech & Simplilearn",
+    timeFrame:"03/2022-09/2022",
+    address:"online",
+    details:"Full Stack Developer course 7 modules comprehensive courses cover large areas in Java, Back end and Front end. Every module ended with a project."
+},
+{
+    id:"2",
+    qualification:"Bachelor of Science in Mathematics Education", 
+    schoolProvider:"Nyiregyhazi University,Hu",
+    timeFrame:"08/1998-07/2023",
+    address:"campus based",
+    details:"Primary Mathematics Teacher to teach in school from 10-14 years old students "
+},
+{
+    id:"3",
+    qualification:"Leaving Certificate", 
+    schoolProvider:"Kossuth Gimnazium",
+    timeFrame:"08/1986-07/1990",
+    address:"campus based",
+    details:"Secondary School with advance level Computer Science classes, basic and turbo pascal languages."
+},
+]
