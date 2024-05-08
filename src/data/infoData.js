d36c70d2f8ceb890077a9f109aad75d69faaf48e
juditanita <101408621+juditanita@users.noreply.github.com>
2024-05-08 15:48:16 +0200

    style about page

---
+++ src/data/infoData.js
@@ -0,0 +1,22 @@
+const infoData=[{
+    id:"1",
+title : "Experience",
+name: ">2 years"
+},
+{id:"2",
+    title: " ➤ Location",
+    name: "Manilva, Spain"
+    },
+    {
+        id:"3",
+        title: "Availibility",
+        name: "1 month"
+        },
+        {
+            id:"4",
+            title: "Relocation",
+            name: "Yes"
+            }
+]
+
+export default infoData;
