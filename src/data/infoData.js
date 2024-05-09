86ef625d3379b09153cbd0d1632377e53b5712a6
juditanita <101408621+juditanita@users.noreply.github.com>
2024-05-09 17:32:37 +0200

    fix styles

--- src/data/infoData.js
+++ src/data/infoData.js
@@ -1,47 +1,94 @@
-export const infoData=[{
-    id:"1",
-title : "Experience",
-name: ">2 years"
-},
-{id:"2",
-    title: " ➤ Location",
-    name: "Manilva, Spain"
+export const headerData=[
+   
+    {
+      href:"/about",
+      name:"About"
+    },
+    
+    {
+      href:"/portfolio",
+      name:"Portfolio"
     },
     {
-        id:"3",
-        title: "Available",
-        name: "1 month"
-        },
-        {
-            id:"4",
-            title: "Relocation",
-            name: "Yes"
-            }
-]
+      href:"/contact",
+      name:"Contact"
+    }
+    ]
 
-export  const educationData=[
-    {
-    id:"1",
-    qualification:"Post Grad in Full Stack Developer", 
-    schoolProvider:"Caltech & Simplilearn",
-    timeFrame:"03/2022-09/2022",
-    address:"online",
-    details:"Full Stack Developer course 7 modules comprehensive courses cover large areas in Java, Back end and Front end. Every module ended with a project."
-},
-{
-    id:"2",
-    qualification:"Bachelor of Science in Mathematics Education", 
-    schoolProvider:"Nyiregyhazi University,Hu",
-    timeFrame:"08/1998-07/2023",
-    address:"campus based",
-    details:"Primary Mathematics Teacher to teach in school from 10-14 years old students "
-},
-{
-    id:"3",
-    qualification:"Leaving Certificate", 
-    schoolProvider:"Kossuth Gimnazium",
-    timeFrame:"08/1986-07/1990",
-    address:"campus based",
-    details:"Secondary School with advance level Computer Science classes, basic and turbo pascal languages."
-},
-]
+
+export const infoData = [
+  {
+    id: '1',
+    title: 'Experience',
+    name: '>2 years',
+  },
+  {
+    id: '2',
+    title: ' ➤ Location',
+    name: 'Manilva, Spain',
+  },
+  {
+    id: '3',
+    title: 'Available',
+    name: '1 month',
+  },
+  {
+    id: '4',
+    title: 'Relocation',
+    name: 'Yes',
+  },
+];
+
+export const educationData = [
+  {
+    id: '1',
+    qualification: 'Post Grad in Full Stack Developer',
+    schoolProvider: 'Caltech & Simplilearn',
+    timeFrame: '03/2022-09/2022',
+    address: 'online',
+    details: 'Full Stack Developer course 7 modules comprehensive courses cover large areas in Java, Back end and Front end. Every module ended with a project.',
+  },
+  {
+    id: '2',
+    qualification: 'Bachelor of Science in Mathematics Education',
+    schoolProvider: 'Nyiregyhazi University,Hu',
+    timeFrame: '08/1998-07/2023',
+    address: 'campus based',
+    details: 'Primary Mathematics Teacher to teach in school from 10-14 years old students ',
+  },
+  {
+    id: '3',
+    qualification: 'Leaving Certificate',
+    schoolProvider: 'Kossuth Gimnazium',
+    timeFrame: '08/1986-07/1990',
+    address: 'campus based',
+    details: 'Secondary School with advance level Computer Science classes, basic and turbo pascal languages.',
+  },
+];
+
+export const experienceData = [
+  {
+    title: 'Learning Experience Full Stack developer',
+    timeFrame: '03/2022-present',
+    address: 'home-based',
+    details: 'Creating customized web applications from scratch, employing a diverse set of programming languages.',
+  },
+  {
+    title: 'School Mathematics Teacher, Private Mathematics Tutor & Nanny',
+    timeFrame: '1990-2022',
+    address: 'varies',
+    details: 'Fulfill unique client, education requirements offering tailored solutions. Monitoring children activities, maintaining relationships with parents. Managing schedules, travel arrangements, and coordinating meetings and events',
+  },
+  {
+    title: 'Server Operator',
+    timeFrame: '12/2005-11/2009',
+    address: 'Dell, Ireland',
+    details: 'Building server computers for enterprises fostering maximum customer satisfaction,  ensuring professionalism in alignments with the values of the company  ',
+  },
+  {
+    title: 'Hostes, Product Demonstator',
+    timeFrame: '03/1992-08/1995',
+    address: 'various events',
+    details: ' Demonstrated deep expertise in the products or services, providing customers with informed recommendations and advice.',
+  },
+];
