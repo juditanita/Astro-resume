d36486632d82b851535e4e18056bb8b0b7b4cee4
juditanita <101408621+juditanita@users.noreply.github.com>
2024-05-13 02:38:39 +0200

    add model for contact

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,24 @@ navLinks.forEach((link) => {
     link.classList.add("active");
   }
 });
+
+
+//see contact model
+
+const openModelBtn=document.querySelector("#contactModel");
+const modelBox=document.querySelector("#contactFormModal");
+const closeModelBtn =document.querySelector("#closeContactForm");
+
+openModelBtn.addEventListener('click', function(){
+  modelBox.classList.remove("hidden")
+  // stop scrolling
+  document.body.classList.add("overflow-y-hidden")
+  
+
+})
+
+closeModelBtn.addEventListener("click", function(){
+  modelBox.classList.add("hidden")
+  document.body.classList.remove("overflow-y-hidden")
+})
+
