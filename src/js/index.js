a8147dcb9d623245490ab1d5579fafbbdaa6fe59
juditanita <101408621+juditanita@users.noreply.github.com>
2024-05-13 13:00:00 +0200

    create contact function

--- src/js/index.js
+++ src/js/index.js
@@ -17,7 +17,7 @@ const openModelBtn=document.querySelector("#contactModel");
 const modelBox=document.querySelector("#contactFormModal");
 const closeModelBtn =document.querySelector("#closeContactForm");
 
-openModelBtn.addEventListener('click', function(){
+openModelBtn?.addEventListener('click', function(){
   modelBox.classList.remove("hidden")
   // stop scrolling
   document.body.classList.add("overflow-y-hidden")
@@ -25,8 +25,34 @@ openModelBtn.addEventListener('click', function(){
 
 })
 
-closeModelBtn.addEventListener("click", function(){
+closeModelBtn?.addEventListener("click", function(){
   modelBox.classList.add("hidden")
   document.body.classList.remove("overflow-y-hidden")
 })
 
+
+//form submiting
+
+const btnSending=document.getElementById("btn-sending"),
+formSending=document.getElementById("submit-form");
+
+
+formSending?.addEventListener('submit',function(e){
+  e.preventDefault();
+
+  btnSending.textContent="Sending now..."
+
+  const serviceID = ASTRO_PUBLIC_SERVICE_ID;
+  const templateID = ASTRO_PUBLIC_TEMPLATE_ID;
+  const publicKey =ASTRO_PUBLIC_PUBLIC_KEY;
+
+
+  emailjs.sendForm(serviceID, templateID, this,publicKey).then(
+    () => {
+       btn.textContent = 'Send Email';
+       alert('Sent!');
+    }, (err) => {
+      btn.textContent = 'Send Email';
+      alert(JSON.stringify(err));
+   })
+})
