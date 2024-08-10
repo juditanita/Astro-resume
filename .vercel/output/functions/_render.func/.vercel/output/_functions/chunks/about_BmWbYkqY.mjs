32f18c089272d20d8c65c71d5684d7efcbaedb51
juditanita <101408621+juditanita@users.noreply.github.com>
2024-08-10 19:37:34 +0200

    update

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/about_BmWbYkqY.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/about_DgonQVgC.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/drivingschool-landingpage_CNlZXRrU.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./drivingschool-landingpage_nraNQ2yi.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
